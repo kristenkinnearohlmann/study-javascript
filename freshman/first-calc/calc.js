989273403360dff75177708597cf09fdd66173d3
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-07-29 20:50:14 -0500

    Started JS portion of the tutorial

---
+++ freshman/first-calc/calc.js
@@ -0,0 +1,15 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay() {
+    // select the element with class of çalculator-screen'
+    const display = document.querySelector('.calculator-screen');
+    // update the value of the element with the contents of 'displayValue'
+    display.value = calculator.displayValue;
+}
+
+updateDisplay();
