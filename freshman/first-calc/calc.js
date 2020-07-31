5f9fde11a48f207610b368cf6ed88ac1ae4cbe7f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-07-30 20:17:05 -0500

    Worked through the key listener for calculator keys

--- freshman/first-calc/calc.js
+++ freshman/first-calc/calc.js
@@ -13,3 +13,33 @@ function updateDisplay() {
 }
 
 updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click',(event) => {
+    // Access the clicked element
+    // The below is the same as const target = event.target;
+    const { target } = event;
+
+    // Check if the clicked element is a button.
+    // If not, exit from the function
+    if (!target.matches('button')) {
+        return;
+    }
+
+    if (target.classList.contains('operator')) {
+        console.log('operator', target.value);
+        return;
+    }
+
+    if (target.classList.contains('decimal')) {
+        console.log('decimal', target.value);
+        return;
+    }
+
+    if (target.classList.contains('all-clear')) {
+        console.log('clear', target.value);
+        return;
+    }
+
+    console.log('digit', target.value);
+});
