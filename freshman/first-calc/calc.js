6ee188f66ca8d287c900eaec58e85d34ed77650b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-08-11 20:46:56 -0500

    Refactored the event listener from the tutorial

--- freshman/first-calc/calc.js
+++ freshman/first-calc/calc.js
@@ -97,6 +97,7 @@ keys.addEventListener('click',(event) => {
     // Access the clicked element
     // The below is the same as const target = event.target;
     const { target } = event;
+    const { value } = target;
 
     // Check if the clicked element is a button.
     // If not, exit from the function
@@ -104,24 +105,26 @@ keys.addEventListener('click',(event) => {
         return;
     }
 
-    if (target.classList.contains('operator')) {
-        handleOperator(target.value);
-        updateDisplay();
-        return;
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'all-clear':
+            resetCalculator();
+            break;
+        default:
+            // check if the key is an integer
+            if (Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+            }
     }
 
-    if (target.classList.contains('decimal')) {
-        inputDecimal(target.value);
-        updateDisplay();
-        return;
-    }
-
-    if (target.classList.contains('all-clear')) {
-        resetCalculator();
-        updateDisplay();
-        return;
-    }
-
-    inputDigit(target.value);
     updateDisplay();
 });
